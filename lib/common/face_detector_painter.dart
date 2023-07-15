3e90384861df78f15651bd60464c0fa0d463b10c
shunsuke-tamura <wwwxx4869@gmail.com>
2023-07-15 17:04:44 +0900

    udpate: 吹き出しのpainter作成

--- lib/common/face_detector_painter.dart
+++ lib/common/face_detector_painter.dart
@@ -59,10 +59,67 @@ class FaceDetectorPainter extends CustomPainter {
         paint1,
       );
     }
+
+    void speechBubble(double faceWidth, Position faceLeftTop) {
+      final width = faceWidth * 1.2;
+      final height = width * 0.5;
+      final pinHeight = height * 0.3;
+      final pinWidth = pinHeight;
+      final bubbleLeftBottom = Position(
+          (faceLeftTop.x + faceWidth / 2) - width / 2,
+          faceLeftTop.y - pinHeight);
+      final bubbleEdgeDiameter = width * 0.1;
+
+      final paint = Paint()
+        ..color = Colors.white
+        ..style = PaintingStyle.fill;
+
+      final path = Path()
+        ..moveTo(bubbleLeftBottom.x, bubbleLeftBottom.y)
+        ..lineTo(
+            bubbleLeftBottom.x + width - bubbleEdgeDiameter, bubbleLeftBottom.y)
+        ..quadraticBezierTo(bubbleLeftBottom.x + width, bubbleLeftBottom.y,
+            bubbleLeftBottom.x + width, bubbleLeftBottom.y - bubbleEdgeDiameter)
+        ..lineTo(bubbleLeftBottom.x + width,
+            bubbleLeftBottom.y - height + bubbleEdgeDiameter)
+        ..quadraticBezierTo(
+            bubbleLeftBottom.x + width,
+            bubbleLeftBottom.y - height,
+            bubbleLeftBottom.x + width - bubbleEdgeDiameter,
+            bubbleLeftBottom.y - height)
+        ..lineTo(bubbleLeftBottom.x + bubbleEdgeDiameter,
+            bubbleLeftBottom.y - height)
+        ..quadraticBezierTo(
+            bubbleLeftBottom.x,
+            bubbleLeftBottom.y - height,
+            bubbleLeftBottom.x,
+            bubbleLeftBottom.y - height + bubbleEdgeDiameter)
+        ..lineTo(bubbleLeftBottom.x, bubbleLeftBottom.y - bubbleEdgeDiameter)
+        ..quadraticBezierTo(bubbleLeftBottom.x, bubbleLeftBottom.y,
+            bubbleLeftBottom.x + bubbleEdgeDiameter, bubbleLeftBottom.y)
+        // pin
+        ..moveTo(
+            bubbleLeftBottom.x + width / 2 - pinWidth / 2, bubbleLeftBottom.y)
+        ..lineTo(bubbleLeftBottom.x + width / 2, bubbleLeftBottom.y + pinHeight)
+        ..lineTo(
+            bubbleLeftBottom.x + width / 2 + pinWidth / 2, bubbleLeftBottom.y);
+
+      canvas.drawPath(path, paint);
+    }
+
+    speechBubble(100, Position(200, 200));
+    speechBubble(300, Position(20, 150));
+    speechBubble(40, Position(100, 400));
   }
 
   @override
   bool shouldRepaint(FaceDetectorPainter oldDelegate) {
     return oldDelegate.imageSize != imageSize || oldDelegate.faces != faces;
   }
+}
+
+class Position {
+  double x;
+  double y;
+  Position(this.x, this.y);
 }
